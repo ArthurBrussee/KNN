09656bc4ae7b7c3db9e7b56ed956c1174bbe64fa
Arthur Brussee <arthur.brussee@gmail.com>
2019-06-11 12:08:47 +0100

    Initial commit

---
+++ KNN/Demo/KnnApiDemo.cs
@@ -0,0 +1,91 @@
+﻿using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+using KNN;
+using Unity.Jobs;
+using Random = Unity.Mathematics.Random;
+
+public class KnnApiDemo : MonoBehaviour  {
+	// Api demo: 
+	void Start() {
+		const int kNeighbours = 10;
+
+		// First let's create a point cloud
+		var points = new NativeArray<float3>(100000, Allocator.Persistent);
+		var rand = new Random(123456);
+		for (int i = 0; i < points.Length; ++i) {
+			points[i] = rand.NextFloat3();
+		}
+		
+		// Create a container that accelerates querying for neighbours
+		var knnContainer = new KnnContainer(points);
+
+		// Create an object that allocates memory necessary for queries
+		// We can't do this in a job so it is seperated out
+		var cache = KnnQueryCache.Create(10);
+
+		// Most basic usage:
+		// Get 10 nearest neighbours as indices into our points array!
+		// This is NOT burst accelerated yet! Waiting for Unity to implement compiling delegates with Burst...
+		var result = new NativeArray<int>(kNeighbours, Allocator.TempJob);
+		knnContainer.KNearest(float3.zero, result, cache);
+		
+		// Get a job to do the query.
+		var queryJob = knnContainer.KNearestAsync(new float3(1.0f, 1.0f, 1.0f), result, cache);
+		
+		// And just run immediatly on the main thread for now. Uses Burst!
+		queryJob.Schedule().Complete();
+
+		// Or maybe we want to query neighbours for multiple points.
+		const int queryPoints = 1024;
+		
+		// Keep an array of neighbour indices of all points
+		var results = new NativeArray<int>(queryPoints * kNeighbours, Allocator.TempJob);
+		var queryPositions = new NativeArray<float3>(queryPoints, Allocator.TempJob);
+
+		for (int i = 0; i < queryPoints; ++i) {
+			queryPositions[i] = rand.NextFloat3() * 0.1f;
+		}	
+
+		var batchQueryJob = knnContainer.KNearestAsync(queryPositions, results, cache);
+
+		// And just run immediatly on main thread for now
+		batchQueryJob.Schedule().Complete();
+
+		// Or maybe we're querying a _ton_ of points (1024 isn't that much but we'll roll with it...)
+		// It's a little cumbersome, but we can multi-thread this ourselves
+		// If anyone finds a way to expose this as a clean API, go for it
+		// But note thar be dragons - tricky to get right! Main trouble is we need a cache per thread...
+		const int jobCount = 8;
+		var handles = new NativeArray<JobHandle>(jobCount, Allocator.TempJob);
+		var caches = new KnnQueryCache[jobCount];
+
+		for (int t = 0; t < jobCount; ++t) {
+			int scheduleRange = queryPositions.Length / jobCount;
+			int start = scheduleRange * t;
+			int end = t == jobCount - 1 ? queryPositions.Length : scheduleRange * (t + 1);
+
+			caches[t] = KnnQueryCache.Create(kNeighbours);
+			var posSlice = queryPositions.Slice(start, end - start);
+			var resultSlice = results.Slice(start * kNeighbours, (end - start) * kNeighbours);
+			var job = knnContainer.KNearestAsync(posSlice, resultSlice, caches[t]);
+			handles[t] = job.Schedule();
+		}
+
+		// Wait for all jobs to be done
+		JobHandle.CompleteAll(handles);
+
+		for (int t = 0; t < jobCount; ++t) {
+			caches[t].Dispose();
+		}
+
+		// Get rid of memory we're using
+		knnContainer.Dispose();
+		handles.Dispose();
+		queryPositions.Dispose();
+		results.Dispose();
+		cache.Dispose();
+		points.Dispose();
+		result.Dispose();
+	}
+}
---
+++ KNN/Demo/KnnVisualizationDemo.cs
@@ -0,0 +1,124 @@
+﻿using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+using KNN;
+using Unity.Jobs;
+using UnityEngine.Experimental.ParticleSystemJobs;
+
+// Ideally you really would use something like ECS to visualize the point cloud
+// And query from a job component system
+// But this is meant to be a really simple demo and not an ECS demo
+// I might look into deeper ECS integration at some point
+public class KnnVisualizationDemo : MonoBehaviour {
+	public int ParticleCount = 20000;
+	public int QueryK = 20;
+	
+	ParticleSystem m_system;
+
+	NativeArray<float3> m_queryPositions;
+	NativeArray<Color32> m_queryColors;
+	
+	NativeArray<float3> m_points;
+	NativeArray<int> m_results;
+	
+	KnnContainer m_container;
+	KnnQueryCache m_cache;
+
+	int frame = 0;
+	
+	void Start() {
+		m_system = GetComponent<ParticleSystem>();
+
+		m_system.Emit(ParticleCount);
+		m_points = new NativeArray<float3>(ParticleCount, Allocator.Persistent);
+
+		m_cache = KnnQueryCache.Create(QueryK);
+		
+		// Create a container that accelerates querying for neighbours
+		m_container = new KnnContainer(m_points, build:false); // Skip building for now. We rebuild every frame
+	}
+
+	void OnDestroy() {
+		m_points.Dispose();
+		m_cache.Dispose();
+		m_container.Dispose();
+		m_results.Dispose();
+		m_queryPositions.Dispose();
+		m_queryColors.Dispose();
+	}
+	
+	// [BurstCompile(CompileSynchronously = true)]
+	struct ParticleJob : IParticleSystemJob {
+		[ReadOnly] public NativeArray<int> KnnResults;
+		public NativeArray<float3> Points;
+
+		public NativeArray<Color32> Colors;
+		public int K;
+		
+		public void ProcessParticleSystem(ParticleSystemJobData jobData) {
+			var colors = jobData.startColors;
+			var positions = jobData.positions;
+			
+			for (int i = 0; i < jobData.count; ++i) {
+				Points[i] = new float3(positions.x[i], positions.y[i], positions.z[i]);
+			}
+			
+			for (int i = 0; i < jobData.count; i++) {
+				colors[i] = new Color32(0, 0, 0, 255);
+			}
+
+			// Set all neighbours to red
+			for (int i = 0; i < KnnResults.Length; i++) {
+				colors[KnnResults[i]] = Colors[i / K];
+			}
+		}
+	}
+
+	void Update() {
+		// Update particles job to do the colors
+		m_system.SetJob(new ParticleJob {
+			KnnResults = m_results,
+			Points = m_points,
+			K = QueryK,
+			Colors = m_queryColors
+		});
+	}
+	
+	// After particle job
+	void LateUpdate() {
+		if (frame < 5) {
+			++frame;
+			return;
+		}	
+
+		// Rebuild our datastructure
+		var rebuild = m_container.RebuildAsync();
+		var rebuildHandle = rebuild.Schedule();
+		
+		// Get all probe positions / colors
+		if (!m_queryPositions.IsCreated || m_queryPositions.Length != QueryProbe.All.Count) {
+			if (m_queryPositions.IsCreated) {
+				m_queryPositions.Dispose();
+				m_results.Dispose();
+				m_queryColors.Dispose();
+			}
+
+			m_queryPositions = new NativeArray<float3>(QueryProbe.All.Count, Allocator.Persistent);
+			m_results = new NativeArray<int>(QueryK * QueryProbe.All.Count, Allocator.Persistent);
+			m_queryColors = new NativeArray<Color32>(QueryProbe.All.Count, Allocator.Persistent);
+		}
+
+		for (int i = 0; i < QueryProbe.All.Count; i++) {
+			var p = QueryProbe.All[i];
+			m_queryPositions[i] = p.transform.position;
+			m_queryColors[i] = p.Color;
+		}
+
+		
+		// Now do the KNN query
+		var query = m_container.KNearestAsync(m_queryPositions, m_results, m_cache);
+		
+		// Schedule query, dependent on the rebuild
+		query.Schedule(rebuildHandle).Complete();
+	}
+}
