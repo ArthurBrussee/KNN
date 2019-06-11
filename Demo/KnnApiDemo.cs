68d57fcc23261b28eaf2de4699550e00beac530f
Arthur Brussee <arthur.brussee@gmail.com>
2019-06-11 23:27:43 +0100

    Add some profiling info to API demo

--- Demo/KnnApiDemo.cs
+++ Demo/KnnApiDemo.cs
@@ -3,10 +3,13 @@ using Unity.Mathematics;
 using KNN;
 using KNN.Jobs;
 using Unity.Jobs;
+using UnityEngine.Profiling;
 using Random = Unity.Mathematics.Random;
 
 public static class KnnApiDemo  {
-	static void Demo() {
+	public static void Demo() {
+		Profiler.BeginSample("Test Query");
+		
 		// First let's create a random point cloud
 		var points = new NativeArray<float3>(100000, Allocator.Persistent);
 		var rand = new Random(123456);
@@ -18,27 +21,34 @@ public static class KnnApiDemo  {
 		const int kNeighbours = 10;
 		float3 queryPosition = float3.zero;
 		
+		Profiler.BeginSample("Build");
 		// Create a container that accelerates querying for neighbours
-		var knnContainer = new KnnContainer(points, true, Allocator.TempJob);
-
+		var knnContainer = new KnnContainer(points, false, Allocator.TempJob);
+		Profiler.EndSample();
+		
+		
 		// Most basic usage:
 		// Get 10 nearest neighbours as indices into our points array!
 		// This is NOT burst accelerated yet! Unity need to implement compiling delegates with Burst
 		var result = new NativeArray<int>(kNeighbours, Allocator.TempJob);
 		knnContainer.KNearest(queryPosition, result);
-		
+
 		// The result array at this point contains indices into the points array with the nearest neighbours!
 		
+		Profiler.BeginSample("Simple Query");
 		// Get a job to do the query.
 		var queryJob = new KNearestQueryJob(knnContainer, queryPosition, result);
 		
 		// And just run immediately on the main thread for now. This uses Burst!
 		queryJob.Schedule().Complete();
+		Profiler.EndSample();
 
+		
 		// Or maybe we want to query neighbours for multiple points.
 		const int queryPoints = 1024;
 		
 		// Keep an array of neighbour indices of all points
+		Profiler.BeginSample("Batch Query");
 		var results = new NativeArray<int>(queryPoints * kNeighbours, Allocator.TempJob);
 		
 		// Query at a few random points
@@ -52,6 +62,8 @@ public static class KnnApiDemo  {
 
 		// And just run immediately now. This will run on multiple threads!
 		batchQueryJob.ScheduleBatch(queryPositions.Length, 128).Complete();
+		Profiler.EndSample();
+		
 		
 		// Now the results array contains all the neighbours!
 		knnContainer.Dispose();
@@ -59,5 +71,6 @@ public static class KnnApiDemo  {
 		results.Dispose();
 		points.Dispose();
 		result.Dispose();
+		Profiler.EndSample();
 	}
 }
