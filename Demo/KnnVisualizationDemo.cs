ff2ebae8b9ed39cc5fb579593e929dc486b7dd62
Joan Llobera <joan.llobera@artanim.ch>
2022-12-06 18:08:12 +0100

    fixed errors related with changes in the way jobs have changed between unity versions, also simplified demo.

--- Demo/KnnVisualizationDemo.cs
+++ Demo/KnnVisualizationDemo.cs
@@ -4,31 +4,34 @@ using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
-//using UnityEngine.Experimental.ParticleSystemJobs;
-//using UnityEngine.Experimental;
+using Unity.Burst;
 
 using UnityEngine.ParticleSystemJobs;
 
 // Ideally you really would use something like ECS to visualize the point cloud
 // And query from a job component system
 // But this is meant to be a really simple demo and not an ECS demo
-// I might look into deeper ECS integration at some point
+
 public class KnnVisualizationDemo : MonoBehaviour {
 	public enum QueryMode {
 		KNearest,
 		Range
 	}
 
-	public QueryMode Mode;
+    //public QueryMode Mode;
+    private QueryMode Mode=QueryMode.KNearest;
 
-	public int ParticleCount = 20000;
+    public int ParticleCount = 20000;
 	
 	public int QueryK = 20;
-	public float QueryRange = 1.0f;
+	//public float QueryRange = 1.0f;
 	
 	ParticleSystem m_system;
+    
+    
+	//private ParticleJob job = new ParticleJob();
 
-	NativeArray<float3> m_queryPositions;
+    NativeArray<float3> m_queryPositions;
 	NativeArray<Color32> m_queryColors;
 
 	NativeArray<float3> m_points;
@@ -46,22 +49,29 @@ public class KnnVisualizationDemo : MonoBehaviour {
 
 		// Create a container that accelerates querying for neighbours
 		m_container = new KnnContainer(m_points, false, Allocator.Persistent); // Skip building for now. We rebuild every frame
-	}
+
+        m_queryColors = new NativeArray<Color32>(QueryProbe.All.Count, Allocator.Persistent);
+        m_results = new NativeArray<int>(QueryK * QueryProbe.All.Count, Allocator.Persistent);
+      //  AssemblyReloadEvents.afterAssemblyReload +=RebuildDataStructure;
+    }
 
 	void OnDestroy() {
-		m_points.Dispose();
+        m_queryColors.Dispose();
+        m_results.Dispose();
+        m_points.Dispose();
 		m_container.Dispose();
-		m_results.Dispose();
+	
 		foreach (var result in m_rangeResults) {
 			result.Dispose();
 		}
 		m_rangeResults.Dispose();
 		m_queryPositions.Dispose();
-		m_queryColors.Dispose();
-	}
+	
 
-    // [BurstCompile(CompileSynchronously = true)]
-    //	struct ParticleJob : IParticleSystemJob {
+
+    }
+
+  //  [BurstCompile(CompileSynchronously = true)]
     struct ParticleJob : IJobParticleSystem
     {
 
@@ -94,7 +104,7 @@ public class KnnVisualizationDemo : MonoBehaviour {
 		}
 	}
 
-    //	struct ParticleRangeJob : IParticleSystemJob {
+  
     struct ParticleRangeJob : IJobParticleSystem
     {
 
@@ -130,29 +140,50 @@ public class KnnVisualizationDemo : MonoBehaviour {
 			}
 		}
 	}
+
+	ParticleJob job;
 	
-	void Update() {
+    void Update() {
 		if (Mode == QueryMode.KNearest) {
-			// Update particles job to do the colors
-			m_system.SetJob(new ParticleJob {
-				KnnResults = m_results,
-				Points = m_points,
-				K = QueryK,
-				Colors = m_queryColors
-			});
-		}
+            // Update particles job to do the colors
+           
+            job = new ParticleJob
+            {
+                KnnResults = m_results,
+                Points = m_points,
+                K = QueryK,
+                Colors = m_queryColors
+            };
+             job.Schedule(m_system);
+           
+        }
 		else {
 			// Update particles job to do the colors
-			m_system.SetJob(new ParticleRangeJob {
-				KnnResults = m_rangeResults,
-				Points = m_points,
-				Colors = m_queryColors
-			});
-		}
-	}
+		
+			Debug.LogError("ERROR. To fix: operation not done due to a Cast problem between m_rangeResults and int, in the call: m_system.SetJob(new ParticleRangeJob {\r\n\t\t\t\tKnnResults = m_rangeResults,\r\n\t\t\t\tPoints = m_points,\r\n\t\t\t\tColors = m_queryColors\r\n\t\t\t});");
+			/*
+            ParticleJob job = new ParticleJob
+            {
+                KnnResults = m_rangeResults,
+                Points = m_points,
+                Colors = m_queryColors
+            };
+            job.Schedule(m_system);
+			*/
+
+
+        }
+      
+    }
 
 	// After particle job
-	void LateUpdate() {
+	void LateUpdate()
+	{
+		
+		RebuildDataStructure();
+	}
+
+	void RebuildDataStructure() { 
 		// Rebuild our datastructure
 		var rebuild = new KnnRebuildJob(m_container);
 		var rebuildHandle = rebuild.Schedule();
@@ -184,27 +215,35 @@ public class KnnVisualizationDemo : MonoBehaviour {
 			m_queryPositions[i] = p.transform.position;
 			m_queryColors[i] = p.Color;
 		}
+		//rebuildHandle.Complete();
 		
 		switch (Mode) {
 			case QueryMode.KNearest: {
 				// Do a KNN query
-				var query = new QueryKNearestBatchJob(m_container, m_queryPositions, m_results);
+				QueryKNearestBatchJob query = new QueryKNearestBatchJob(m_container, m_queryPositions, m_results);
 
-				// Schedule query, dependent on the rebuild
-				// We're only doing a very limited number of points - so allow each query to have it's own job
-				query.ScheduleBatch(m_queryPositions.Length, 1, rebuildHandle).Complete();
-				break;
+				
+                    // Schedule query, dependent on the rebuild
+                    // We're only doing a very limited number of points - so allow each query to have it's own job
+                query.ScheduleBatch(m_queryPositions.Length, 1, rebuildHandle).Complete();
+
+                   
+
+                    break;
 			}
 
+
+			/*
 			case QueryMode.Range: {
 				// Do a range query
-				var query = new QueryRangeBatchJob(m_container, m_queryPositions, QueryRange, m_rangeResults);
+				///var query = new QueryRangeBatchJob(m_container, m_queryPositions, QueryRange, m_rangeResults);
 			
 				// Schedule query, dependent on the rebuild
 				// We're only doing a very limited number of points - so allow each query to have it's own job
-				query.ScheduleBatch(m_queryPositions.Length, 1, rebuildHandle).Complete();
+				///query.ScheduleBatch(m_queryPositions.Length, 1, rebuildHandle).Complete();
 				break;
-			}
+			}*/
 		}
 	}
+
 }
